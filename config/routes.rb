271d244d7bab89fe12c9307d0d6db24b2f9e986e
藤枝　龍紀 <fujiedaryuuki@fujiedaryuukinoMacBook-Air.local>
2020-07-19 20:43:58 +0900

    sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root 'welcome#top'
+  resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  
+
 end
