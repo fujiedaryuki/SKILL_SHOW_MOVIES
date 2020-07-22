1c8e080bc2af077c2df3ca3de79db1b35a7172e1
藤枝　龍紀 <fujiedaryuuki@fujiedaryuukinoMacBook-Air.local>
2020-07-22 14:20:37 +0900

    flash_messages

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,9 +4,10 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: 'ログインしました'
     else
-      render :new
+      flash.now[:danger] = 'ログインに失敗'
+      render :new 
     end
   end
 
