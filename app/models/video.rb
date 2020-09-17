9033f25894fae010a09cf68ad516082acaf8e05f
藤枝　龍紀 <fujiedaryuuki@fujiedaryuukinoMacBook-Air.local>
2020-09-17 15:40:30 +0900

    動画投稿機能修正

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,9 +6,4 @@ class Video < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
   validates :video_image, presence: { message: 'を選択してください' }
-
-  validates :rate, numericality: {
-    less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1
-  }, presence: true
 end
