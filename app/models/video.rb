e71f9f5c616ea5c21138b6924d3d8db2171cebae
藤枝　龍紀 <fujiedaryuuki@fujiedaryuukinoMacBook-Air.local>
2020-09-21 15:48:22 +0900

    ゲストユーザー変更

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,4 @@ class Video < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
-  validates :video_image, presence: { message: 'を選択してください' }
-
 end
